06109aafa9accb59046ee45350478cb4245e0783
MultipleCrashes <MultipleCrashes@users.noreply.github.com>
2017-09-01 18:37:35 +0530

    Update setup_angular.sh

--- setup_angular.sh
+++ setup_angular.sh
@@ -1,5 +1,5 @@
-#sudo apt-get install -y curl 
-#sudo apt-get install -y python-software-properties
-#curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
-#sudo apt-get install -y nodejs
+sudo apt-get install -y curl 
+sudo apt-get install -y python-software-properties
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+sudo apt-get install -y nodejs
 npm install -g @angular/cli 
